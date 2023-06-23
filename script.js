11cbdf460f8a1a9ded77b4d5615ea18cb073b4a9
Ronin1702 <kchen@me.com>
2023-06-22 23:53:51 -0400

    created css for when screen<992 then display sms

--- script.js
+++ script.js
@@ -3,14 +3,5 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-window.onload = function() {
-  var isMac = navigator.platform.toUpperCase().indexOf('MAC')>=0;
-  if(isMac){
-      var mobileLink = document.getElementById('iMessage');
-      mobileLink.classList.remove('invisible');
-  } return;
-};
-
-
 
 
